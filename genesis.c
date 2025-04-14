c59eb814ba49d2eb0044371df833a6e3f6a1f04e
Lalnuntluanga <tluanga.ca@gmail.com>
2025-04-15 03:50:25 +0530

    The C section

---
+++ genesis.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("1. In the beginning, the Engineer created the Hardware and the Software.\n");
+    printf("2. The Hardware was without form and void, and the system was over the surface of the processor.\n");
+    printf("3. And the Engineer said, 'Let there be a Clock,' and there was the Clock.\n");
+    printf("4. And the Engineer saw that the Clock was good, and the system began to run.\n");
+    return 0;
+}
+
