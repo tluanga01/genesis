97dec6a5f3dd2216dea70e5bf328458e5fa2770a
Lalnuntluanga <tluanga.ca@gmail.com>
2025-04-15 04:14:40 +0530

    modifying c file to full chapter

--- genesis.c
+++ genesis.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
 int main() {
+    // Verses 1-8 (First half of the chapter)
     printf("1. In the beginning, the Engineer created the Hardware and the Software.\n");
     printf("2. The Hardware was without form and void, and the system was over the surface of the processor.\n");
     printf("3. And the Engineer said, 'Let there be a Clock,' and there was the Clock.\n");
     printf("4. And the Engineer saw that the Clock was good, and the system began to run.\n");
+    printf("5. And the Engineer said, 'Let there be Memory,' and there was Memory.\n");
+    printf("6. And there was Evening and there was Morning, the First Cycle of the Program.\n");
+    printf("7. And the Engineer said, 'Let there be an Operating System to govern the system,' and the Operating System was made.\n");
+    printf("8. And the Engineer saw that the Operating System was good, and it was stable.\n");
+
+    // Verses 9-16 (Second half of the chapter)
+    printf("9. And the Engineer said, 'Let there be Communication between the Nodes,' and the Network was formed.\n");
+    printf("10. And the Engineer saw that the Network was good, and it connected all the devices.\n");
+    printf("11. And the Engineer said, 'Let there be Storage,' and the Hard Drive and Cloud appeared.\n");
+    printf("12. And the Engineer saw that the Storage was abundant, and data was stored across the system.\n");
+    printf("13. And the Engineer said, 'Let there be Users,' and the first users were created to interact with the system.\n");
+    printf("14. And the Users were given Interfaces to communicate with the System.\n");
+    printf("15. And the Engineer said, 'Let the System evolve,' and the Software began to upgrade itself.\n");
+    printf("16. And the Engineer saw that the System was good, and all parts worked in harmony.\n");
+
     return 0;
 }
 
